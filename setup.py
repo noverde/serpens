11f8e716f0d7d7ec9582b97b134f4184772ca388
Everaldo Canuto <everaldo.canuto@gmail.com>
2020-11-17 22:08:59 -0300

    Add Python package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="serpens",
+    version="0.1.0",
+    description='A set of Python utilities, recipes and snippets',
+
+    author='Everaldo Canuto',
+    author_email='everaldo.canuto@gmail.com',
+
+    license='MIT',
+    platforms='any',
+
+    packages=find_packages(),
+    install_requires=[],
+)
