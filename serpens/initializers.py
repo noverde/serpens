73af5743af7608adc192645584252a08d0a5bd01
Everaldo Canuto <everaldo.canuto@gmail.com>
2020-11-26 07:51:43 -0300

    Code style Improvements on init_sentry

--- serpens/initializers.py
+++ serpens/initializers.py
@@ -58,10 +58,27 @@ def init_sentry() -> None:
     # sentry-sdk already check for SENTRY_DSN, SENTRY_ENVIRONMENT and
     # SENTRY_RELEASE environment vars so we call check for it to keep
     # compatibility with default behavior.
+    environment = os.getenv(
         "SENTRY_ENVIRONMENT",
+        os.getenv(
+            "ENVIRONMENT",
+            "development"
+        )
+    )
+
+    release = os.getenv(
+        "SENTRY_RELEASE",
+        os.getenv(
+            "RELEASE",
+            "app@0.0.0"
+        )
+    )
+
     sentry_sdk.init(
         dsn=os.getenv("SENTRY_DSN", ""),
+        environment=environment,
         release=release,
+        debug=os.getenv("DEBUG", "False").lower() in ("yes", "true", "1")
     )
     logger.info("Sentry's SDK initialized")
 
