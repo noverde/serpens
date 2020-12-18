7e03017a0f10d472a41e7adad177c35bf2f49d2c
Everaldo Canuto <everaldo.canuto@gmail.com>
2020-12-18 09:58:09 -0300

    Add keyname on get secrets

--- serpens/secrets.py
+++ serpens/secrets.py
@@ -4,11 +4,14 @@ from json import JSONDecodeError
 import boto3
 
 
-def get(secret_id):
+def get(secret_id, keyname=None):
     client = boto3.client("secretsmanager")
     secret_value = client.get_secret_value(SecretId=secret_id)
     secret = secret_value["SecretString"]
     try:
-        return json.loads(secret)
+        result = json.loads(secret)
+        if keyname:
+            return result["keyname"]
+        return result
     except JSONDecodeError:
         return secret
