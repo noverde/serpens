8e6cb5d3b0ece37aaea7f02dc8191c540490252c
João Vitor Stein <joao.stein@noverde.com.br>
2020-12-22 12:52:59 -0300

    remove print

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,5 +13,4 @@ def test_database():
     db = database.setup("sqlite://test.db")
     db.execute("INSERT INTO foo (id, bar) VALUES (1, 'test')")
     result = db.get("SELECT * FROM foo WHERE id = 1")
-    print(result)
     assert result is not None
