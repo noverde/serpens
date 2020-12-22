3db90d55e0994e949177b8eb2f52a2ac3f49c49a
Juliano Fernandes <julianofernandes@gmail.com>
2020-12-22 12:05:15 -0300

    wip: improve api handler

--- serpens/api.py
+++ serpens/api.py
@@ -1,21 +1,18 @@
+import json
+from dataclasses import dataclass
 from functools import wraps
-from typing import Any, Callable, Dict
 
 
-def handler(func: Callable) -> Callable:
+def handler(func):
     @wraps(func)
-    def wrapper(
-        event: Dict[str, Any],
-        context: Dict[str, Any],
-        *args: Any,
-        **kwargs: Any
-    ) -> Dict[str, Any]:
+    def wrapper(event, context):
         response = {
             "headers": {"Access-Control-Allow-Origin": "*"},
             "statusCode": 200,
             "body": "",
         }
-        result = func(event, context)
+        request = Request(event)
+        result = func(request)
 
         if isinstance(result, tuple) and isinstance(result[0], int):
             response["statusCode"] = result[0]
@@ -26,3 +23,30 @@ def handler(func: Callable) -> Callable:
         return response
 
     return wrapper
+
+
+@dataclass
+class Authorizer:
+    partner_id: int = None
+    product_id: int = None
+    borrower_id: int = None
+
+
+class Request:
+    def __init__(self, data):
+        self.data = data
+        self.authorizer = self._authorizer()
+        self.body = self._body()
+
+    def _authorizer(self):
+        context = self.data.get("requestContext") or {}
+        partner_id = context.get("authorizer").get("partner_id")
+        product_id = context.get("authorizer").get("product_id")
+        borrower_id = context.get("authorizer").get("borrower_id")
+        Authorizer(partner_id, product_id, borrower_id)
+
+    def _body(self):
+        try:
+            return json.loads(self.data)
+        except json.JSONDecodeError:
+            return self.data
