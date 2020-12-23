b33d6446a76006886742f791022b4df8bd8a7337
Juliano Fernandes <juliano.fernandes@noverde.com.br>
2020-12-23 09:39:35 -0300

    feat: improve api handler (#5)
    ### Contain
    - [x] Tests
    - [x] Breaking changes
    
    ### Details
    * Update api handler to inject a request object that contains the full event data, an attribute called `authorizer` and another called `body`. The `authorizer` maps whatever came inside the requestContext->authorizer, and the body returns a dict if the content is "json serializable", otherwise the content as is.

--- serpens/api.py
+++ serpens/api.py
@@ -1,21 +1,17 @@
+import json
 from functools import wraps
-from typing import Any, Callable, Dict
 
 
-def handler(func: Callable) -> Callable:
+def handler(func):
     @wraps(func)
-    def wrapper(
-        event: Dict[str, Any],
-        context: Dict[str, Any],
-        *args: Any,
-        **kwargs: Any
-    ) -> Dict[str, Any]:
+    def wrapper(event, context):
         response = {
             "headers": {"Access-Control-Allow-Origin": "*"},
             "statusCode": 200,
             "body": "",
         }
-        result = func(event, context)
+        request = Request(event)
+        result = func(request)
 
         if isinstance(result, tuple) and isinstance(result[0], int):
             response["statusCode"] = result[0]
@@ -26,3 +22,31 @@ def handler(func: Callable) -> Callable:
         return response
 
     return wrapper
+
+
+class Authorizer:
+    def __init__(self, data):
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class Request:
+    def __init__(self, data):
+        self.data = data
+        self.authorizer = self._authorizer()
+        self.body = self._body()
+
+    def _authorizer(self):
+        context = self.data.get("requestContext") or {}
+        authorizer = context.get("authorizer") or {}
+        return Authorizer(authorizer)
+
+    def _body(self):
+        body = self.data.get("body") or ""
+        try:
+            return json.loads(body)
+        except json.JSONDecodeError:
+            return body
