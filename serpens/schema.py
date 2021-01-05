941b02264031e58b8adae156354e3e5a5602b615
Juliano Fernandes <juliano.fernandes@noverde.com.br>
2021-01-05 17:36:36 -0300

    feat: add load, loads, dump and dumps methods for schemas (#8)
    ### Contain
    - [x] New feature
    - [x] Tests
    
    ### Details
    * Add methods to encode and decode data converting from json or json strings to an instance of the schema, and from an instance to a json (serializable) or json string.

--- serpens/schema.py
+++ serpens/schema.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass, fields
+import json
+from copy import deepcopy
+from dataclasses import asdict, dataclass, fields, is_dataclass
+from datetime import date, datetime, time
+from decimal import Decimal
+from enum import Enum
+from uuid import UUID
 
 
 @dataclass
@@ -11,3 +17,54 @@ class Schema:
                 errors.append(msg)
         if errors:
             raise TypeError(*errors)
+
+    @classmethod
+    def load(cls, dictionary, many=False):
+        data = deepcopy(dictionary)
+
+        if many:
+            return list(map(cls.load, data))
+
+        for field in fields(cls):
+            if field.name not in data or data[field.name] is None:
+                continue
+            # cast special types
+            if field.type in (date, datetime, time):
+                data[field.name] = field.type.fromisoformat(data[field.name])
+            elif field.type in (Decimal, UUID):
+                data[field.name] = field.type(data[field.name])
+            elif issubclass(field.type, Enum):
+                data[field.name] = field.type(data[field.name])
+            elif is_dataclass(field.type):
+                data[field.name] = field.type.load(data[field.name])
+
+        return cls(**data)
+
+    @classmethod
+    def loads(cls, json_string, many=False):
+        data = json.loads(json_string)
+        return list(map(cls.load, data)) if many else cls.load(data)
+
+    @classmethod
+    def dump(cls, instance, many=False):
+        string = cls.dumps(instance, many)
+        return json.loads(string)
+
+    @classmethod
+    def dumps(cls, instance, many=False):
+        data = list(map(asdict, instance)) if many else asdict(instance)
+        return json.dumps(data, cls=SchemaEncoder)
+
+
+class SchemaEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # cast special types
+        if isinstance(obj, (date, datetime, time)):
+            return obj.isoformat()
+        elif isinstance(obj, Decimal):
+            return float(obj)
+        elif isinstance(obj, UUID):
+            return str(obj)
+        elif isinstance(obj, Enum):
+            return obj.value
+        return super().default(obj)
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,7 +1,19 @@
+import json
 import unittest
 from dataclasses import dataclass, field
+from datetime import date
+from decimal import Decimal
+from enum import Enum
+from uuid import UUID
 
 from schema import Schema
+
+
+class Level(Enum):
+    INTERN = "intern"
+    JUNIOR = "junior"
+    MIDDLE = "middle"
+    SENIOR = "senior"
 
 
 @dataclass
@@ -9,6 +21,16 @@ class PersonSchema(Schema):
     name: str
     age: int
     hobby: list = field(default_factory=list)
+
+
+@dataclass
+class EmployeeSchema(Schema):
+    person: PersonSchema
+    uid: UUID
+    office: str
+    salary: Decimal
+    level: Level
+    registered: date
 
 
 class TestSchema(unittest.TestCase):
@@ -32,3 +54,105 @@ class TestSchema(unittest.TestCase):
             PersonSchema("foo", "bar")
 
         self.assertEqual(error.exception.args, expected)
+
+    def test_load(self):
+        data = {
+            "person": {"name": "John Doe", "age": 30, "hobby": ["walk"]},
+            "uid": "dc675e20-6e8b-4b05-a8ce-4459560526c3",
+            "office": "main",
+            "salary": 6500.1,
+            "level": "middle",
+            "registered": "2021-01-01",
+        }
+        instance = EmployeeSchema.load(data)
+
+        self.assertIsInstance(instance, EmployeeSchema)
+        self.assertIsInstance(instance.person, PersonSchema)
+        self.assertIsInstance(instance.salary, Decimal)
+        self.assertIsInstance(instance.level, Enum)
+        self.assertIsInstance(instance.registered, date)
+
+    def test_load_many(self):
+        data = [
+            {
+                "person": {"name": "John Doe", "age": 30, "hobby": ["walk"]},
+                "uid": "dc675e20-6e8b-4b05-a8ce-4459560526c3",
+                "office": "main",
+                "salary": 6500.1,
+                "level": "middle",
+                "registered": "2021-01-01",
+            }
+        ]
+        instances = EmployeeSchema.load(data, many=True)
+
+        self.assertIsInstance(instances, list)
+        self.assertIsInstance(instances[0], EmployeeSchema)
+        self.assertIsInstance(instances[0].person, PersonSchema)
+        self.assertIsInstance(instances[0].salary, Decimal)
+        self.assertIsInstance(instances[0].level, Enum)
+        self.assertIsInstance(instances[0].registered, date)
+
+    def test_loads(self):
+        data = json.dumps(
+            {
+                "person": {"name": "John Doe", "age": 30, "hobby": ["walk"]},
+                "uid": "dc675e20-6e8b-4b05-a8ce-4459560526c3",
+                "office": "main",
+                "salary": 6500.1,
+                "level": "middle",
+                "registered": "2021-01-01",
+            }
+        )
+        instance = EmployeeSchema.loads(data)
+
+        self.assertIsInstance(instance, EmployeeSchema)
+        self.assertIsInstance(instance.person, PersonSchema)
+        self.assertIsInstance(instance.salary, Decimal)
+        self.assertIsInstance(instance.level, Enum)
+        self.assertIsInstance(instance.registered, date)
+
+    def test_dump(self):
+        expected = {
+            "person": {"name": "John Doe", "age": 30, "hobby": ["walk"]},
+            "uid": "dc675e20-6e8b-4b05-a8ce-4459560526c3",
+            "office": "main",
+            "salary": 6500.1,
+            "level": "middle",
+            "registered": "2021-01-01",
+        }
+        instance = EmployeeSchema.load(expected)
+        data = EmployeeSchema.dump(instance)
+
+        self.assertDictEqual(data, expected)
+
+    def test_dump_many(self):
+        expected = [
+            {
+                "person": {"name": "John Doe", "age": 30, "hobby": ["walk"]},
+                "uid": "dc675e20-6e8b-4b05-a8ce-4459560526c3",
+                "office": "main",
+                "salary": 6500.1,
+                "level": "middle",
+                "registered": "2021-01-01",
+            }
+        ]
+        instances = EmployeeSchema.load(expected, many=True)
+        data = EmployeeSchema.dump(instances, many=True)
+
+        self.assertIsInstance(data, list)
+        self.assertDictEqual(data[0], expected[0])
+
+    def test_dumps(self):
+        expected = {
+            "person": {"name": "John Doe", "age": 30, "hobby": ["walk"]},
+            "uid": "dc675e20-6e8b-4b05-a8ce-4459560526c3",
+            "office": "main",
+            "salary": 6500.1,
+            "level": "middle",
+            "registered": "2021-01-01",
+        }
+        instance = EmployeeSchema.load(expected)
+        string = EmployeeSchema.dumps(instance)
+
+        self.assertIsInstance(string, str)
+        self.assertDictEqual(json.loads(string), expected)
