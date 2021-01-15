52349f1eff05ae14600b65a55e0e9a8f23facf99
Jael Zela <jael.zela@noverde.com.br>
2021-01-15 18:05:25 -0300

    refactoring sns publish message

--- serpens/sns.py
+++ serpens/sns.py
@@ -1,37 +1,14 @@
 import json
-from datetime import datetime
-from uuid import uuid4
 
 import boto3
 
-from serpens import envvars
 
-
-def publish_message(topic_arn, category, event_type, aggregate_id, payload):
-
+def publish_message(topic_arn, message, attributes={}):
     sns_client = boto3.client("sns")
-
-    message = {
-        "default": json.dumps(
-            {
-                "id": str(uuid4()),
-                "category": category,
-                "type": event_type,
-                "at": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                "aggregate_id": aggregate_id,
-                "version": "1.0",
-                "payload": payload,
-            }
-        )
-    }
-
     response = sns_client.publish(
         TargetArn=topic_arn,
         Message=json.dumps(message),
         MessageStructure="json",
-        MessageAttributes={
-            "event_type": {"DataType": "String", "StringValue": event_type}
-        },
+        MessageAttributes=attributes,
     )
-
     return response
