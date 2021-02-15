b7ea7ccf1d2f2c232ca8d2d436de7edf78ff9e89
Joao Stein <joao.stein@noverde.com.br>
2021-02-15 10:34:32 -0300

    add fifo support in sqs publish_mesasge

--- serpens/sqs.py
+++ serpens/sqs.py
@@ -1,6 +1,13 @@
 import boto3
 
-def publish_message(queue_url, body):
+def publish_message(queue_url, body, message_group_id=None):
     client = boto3.client('sqs')
-    response = client.send_message(QueueUrl=queue_url, MessageBody=body)
+    params = {
+        "QueueUrl": queue_url,
+        "MessageBody": body
+    }
+    if queue_url.endswith(".fifo"):
+        params["MessageGroupId"] = message_group_id
+        params["MessageDeduplicationId"] = message_group_id
+    response = client.send_message(**params)
     return response
