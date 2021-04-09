00dce5b40c994ee063bfd9b5fd8b7bd0801d3aed
Joao Stein <joao.stein@noverde.com.br>
2021-04-09 15:36:20 -0300

    improving code

--- serpens/cache.py
+++ serpens/cache.py
@@ -12,25 +12,30 @@ def cached(cache_name, ttl_in_seconds):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            cache_key = tuple(args) + tuple(kwargs)
+            cache_key = tuple(args) + tuple(f"{k}={v}" for k, v in kwargs.items())
             cached = cache.get(cache_name, {})
+
             if cache_key in cached and cached[cache_key]["expires_at"] > datetime.now():
-                print("got from cache")
-                logger.debug(f"Getting value with key {cache_key} from cache {cache_name}")
+                logger.debug(
+                    f"Getting value with key {cache_key} from cache {cache_name}"
+                )
                 return cached[cache_key]
-            print("calling function")
+
             result = func(*args, **kwargs)
+
             cache[cache_name] = cached
             cache[cache_name][cache_key] = {
                 "value": result,
-                "expires_at": datetime.now() + timedelta(seconds=ttl_in_seconds)
+                "expires_at": datetime.now() + timedelta(seconds=ttl_in_seconds),
             }
+
             return result
+
         return wrapper
-    
+
     return decorator
 
 
 def clear_cache(cache_name):
     logger.debug(f"Cleaning cache with name {cache_name}")
-    cache[cache_name] = {}
+    cache.pop(cache_name, None)
