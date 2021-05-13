a20653d0a525cbdb8ccc78cc81700f3960d14499
fernandochimi-noverde <fernando.chimicoviaki@noverde.com.br>
2021-05-13 14:58:31 -0300

    feat: Insert cache in `parameters.py`

--- serpens/parameters.py
+++ serpens/parameters.py
@@ -1,6 +1,9 @@
 import boto3
 
+from cache import cached
 
+
+@cached("parameters", 900)
 def get(key) -> str:
     ssm = boto3.client("ssm")
     result = ssm.get_parameter(Name=key, WithDecryption=True)
