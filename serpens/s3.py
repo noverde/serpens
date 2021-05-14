2edeeefb1ed07b406373724a6316b7cee69899e5
Jael Zela <jael.zela@noverde.com.br>
2021-05-14 12:20:13 -0300

    feat: add s3 functions

--- serpens/s3.py
+++ serpens/s3.py
@@ -9,3 +9,22 @@ def upload_file(data, bucket, key, content_type, acl="private"):
     )
 
     return response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+def get_file(bucket, key):
+    client = boto3.client("s3")
+
+    try:
+        response = client.get_object(Bucket=bucket, Key=key)
+    except Exception:
+        return None
+
+    return response.get("Body")
+
+
+def exists(bucket, key):
+    client = boto3.client("s3")
+
+    response = client.list_objects_v2(Bucket=bucket, Prefix=key, MaxKeys=1)
+
+    return "Contents" in response
