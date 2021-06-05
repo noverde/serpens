b04cd82d4a9fd3556f37c183b91115c48e1d3366
Everaldo Canuto <everaldo.canuto@gmail.com>
2021-06-05 18:53:56 -0300

    Test api handler with status code

--- tests/test_api.py
+++ tests/test_api.py
@@ -82,7 +82,21 @@ class TestApiHandler(unittest.TestCase):
             res = {"foo": request.authorizer.foo, "ping": request.body["ping"]}
             return json.dumps(res)
 
+        @api.handler
+        def handler_with_status(request):
+            res = {"foo": request.authorizer.foo, "ping": request.body["ping"]}
+            return 200, json.dumps(res)
+
         response = handler(event, context)
+
+        self.assertIn("headers", response)
+        self.assertIn("statusCode", response)
+        self.assertIn("body", response)
+        self.assertEqual(response["headers"], expected["headers"])
+        self.assertEqual(response["statusCode"], expected["statusCode"])
+        self.assertDictEqual(json.loads(response["body"]), expected["body"])
+
+        response = handler_with_status(event, context)
 
         self.assertIn("headers", response)
         self.assertIn("statusCode", response)
