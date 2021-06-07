725973ecf326ee6f89d337e416b9d4fb62ae00a9
Everaldo Canuto <everaldo.canuto@gmail.com>
2021-06-07 14:44:22 -0300

    Add csvutils tests

--- tests/test_csvutils.py
+++ tests/test_csvutils.py
@@ -1,41 +1,46 @@
+import unittest
 import tempfile
+import warnings
+
 from serpens import csvutils
 
 
-def test_utf8_with_bom():
-    reader = csvutils.open_csv_reader("tests/samples/fruits_utf8bom.csv")
+class TestCsvUtils(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter(action="ignore", category=ResourceWarning)
 
-    assert reader.fieldnames == ["id", "name"]
+    def test_utf8_with_bom(self):
+        reader = csvutils.open_csv_reader("tests/samples/fruits_utf8bom.csv")
 
-    line = next(reader)
-    assert line == {"id": "1", "name": "Açaí"}
+        self.assertEqual(reader.fieldnames, ["id", "name"])
 
+        line = next(reader)
+        self.assertEqual(line, {"id": "1", "name": "Açaí"})
 
-def test_utf8_without_bom():
-    reader = csvutils.open_csv_reader("tests/samples/fruits_utf8.csv")
-    assert reader.fieldnames == ["id", "name"]
+    def test_utf8_without_bom(self):
+        reader = csvutils.open_csv_reader("tests/samples/fruits_utf8.csv")
+        self.assertEqual(reader.fieldnames, ["id", "name"])
 
-    line = next(reader)
-    assert line == {"id": "1", "name": "Açaí"}
+        line = next(reader)
+        self.assertEqual(line, {"id": "1", "name": "Açaí"})
 
+    def test_iso8859(self):
+        reader = csvutils.open_csv_reader("tests/samples/fruits_iso8859.csv")
+        self.assertEqual(reader.fieldnames, ["id", "name"])
 
-def test_iso8859():
-    reader = csvutils.open_csv_reader("tests/samples/fruits_iso8859.csv")
-    assert reader.fieldnames == ["id", "name"]
+        line = next(reader)
+        self.assertEqual(line, {"id": "1", "name": "Açaí"})
+        del reader
 
-    line = next(reader)
-    assert line == {"id": "1", "name": "Açaí"}
+    def test_writer(self):
+        _, temp = tempfile.mkstemp()
+        writer = csvutils.open_csv_writer(temp)
+        writer.writerow(["id", "name"])
+        writer.writerow(["1", "Açaí"])
+        del writer
 
+        reader = csvutils.open_csv_reader(temp)
+        self.assertEqual(reader.fieldnames, ["id", "name"])
 
-def test_writer():
-    _, temp = tempfile.mkstemp()
-    writer = csvutils.open_csv_writer(temp)
-    writer.writerow(["id", "name"])
-    writer.writerow(["1", "Açaí"])
-    del writer
-
-    reader = csvutils.open_csv_reader(temp)
-    assert reader.fieldnames == ["id", "name"]
-
-    line = next(reader)
-    assert line == {"id": "1", "name": "Açaí"}
+        line = next(reader)
+        self.assertEqual(line, {"id": "1", "name": "Açaí"})
