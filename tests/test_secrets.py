fea3cd84b8d3f7c4dd088ccaf0425c72db8a0d83
Juliano Fernandes <julianofernandes@gmail.com>
2021-06-07 14:53:34 -0300

    test: add tests for secrets and documents modules

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -1,25 +1,36 @@
 import json
+import secrets
 import unittest
 from unittest.mock import patch
-
-import secrets
 
 
 class TestSecrets(unittest.TestCase):
     @patch("secrets.boto3")
     def test_retrieve_secret_value_without_keyname(self, m_boto3):
+        aws_response = {"SecretString": '{"key": "s3CrE7-V@1u3"}'}
+        m_boto3.client.return_value.get_secret_value.return_value = aws_response
+
+        secret_value = secrets.get("Secret_Value")
+
+        self.assertDictEqual(secret_value, json.loads(aws_response["SecretString"]))
+        self.assertEqual(m_boto3.client.call_count, 1)
+
+    @patch("secrets.boto3")
+    def test_retrieve_secret_value_with_invalid_json(self, m_boto3):
         aws_response = {"SecretString": "s3CrE7-V@1u3"}
         m_boto3.client.return_value.get_secret_value.return_value = aws_response
-        secret_value = secrets.get("Secret_Value")
+
+        secret_value = secrets.get("Another_Secret_Value")
+
         self.assertEqual(secret_value, aws_response["SecretString"])
-        secrets.get("Secret_Value")
         self.assertEqual(m_boto3.client.call_count, 1)
 
     @patch("secrets.boto3")
     def test_retrieve_secret_value_with_keyname(self, m_boto3):
         aws_response = {"SecretString": '{"key": "s3CrE7-V@1u3"}'}
         m_boto3.client.return_value.get_secret_value.return_value = aws_response
-        secret_value = secrets.get("Secret_Value", "key")
+
+        secret_value = secrets.get("Another_Secret_Value", "key")
+
         self.assertEqual(secret_value, json.loads(aws_response["SecretString"])["key"])
-        secrets.get("Secret_Value", "key")
         self.assertEqual(m_boto3.client.call_count, 1)
