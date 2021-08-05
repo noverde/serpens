68ff5766d3ca6a65cec080cef1455e780b449f2f
Igor Rodrigues <igorrss652@gmail.com>
2021-08-05 10:07:05 -0300

    Update version in setup.py using variables from settings

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from serpens.settings import APPNAME, VERSION
 
 setup(
-    name="serpens",
-    version="1.11.0",
+    name=APPNAME,
+    version=VERSION,
     description="A set of Python utilities, recipes and snippets",
     author="Everaldo Canuto",
     author_email="everaldo.canuto@gmail.com",
