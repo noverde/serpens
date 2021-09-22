4ca8c68bd71c47a631e18bbb5070c6a91212df39
Igor <igorrss652@gmail.com>
2021-09-22 13:58:14 -0300

    Fix sending duplicate messages to sentry (#42)
    * Treat api handler exception with logger.exception
    
    * Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="serpens",
-    version="1.13.0",
+    version="1.13.1",
     description="A set of Python utilities, recipes and snippets",
     author="Everaldo Canuto",
     author_email="everaldo.canuto@gmail.com",
