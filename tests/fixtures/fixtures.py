80852a6dd79b8b1b42a0761be081543977fdbb46
Igor <igorrss652@gmail.com>
2021-10-20 08:53:27 -0300

    Add tests to classmethods cache and instance methods cache. (#47)

---
+++ tests/fixtures/fixtures.py
@@ -0,0 +1,18 @@
+import cache
+
+
+class ClsCached:
+
+    counter_cls = 0
+    counter_self = 0
+
+    @classmethod
+    @cache.cached("test_cache_cls", 900)
+    def proc_cls(cls):
+        cls.counter_cls += 1
+        return cls.counter_cls
+
+    @cache.cached("test_cache_self", 900)
+    def proc_self(self):
+        self.counter_self += 1
+        return self.counter_self
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,6 +1,7 @@
+import cache
 import unittest
 
-import cache
+from tests.fixtures.fixtures import ClsCached
 
 
 class TestSCache(unittest.TestCase):
@@ -26,3 +27,29 @@ class TestSCache(unittest.TestCase):
 
         strike2 = self.get_cached_counter()
         self.assertEqual(strike2, 2)
+
+    def test_cache_classmethod(self):
+        strike1 = ClsCached.proc_cls()
+        self.assertEqual(strike1, 1)
+
+        strike2 = ClsCached.proc_cls()
+        self.assertEqual(strike2, 1)
+
+        cache.clear_cache("test_cache_cls")
+
+        strike3 = ClsCached.proc_cls()
+        self.assertEqual(strike3, 2)
+
+    def test_cache_method(self):
+        obj = ClsCached()
+
+        strike1 = obj.proc_self()
+        self.assertEqual(strike1, 1)
+
+        strike2 = obj.proc_self()
+        self.assertEqual(strike2, 1)
+
+        cache.clear_cache("test_cache_self")
+
+        strike3 = obj.proc_self()
+        self.assertEqual(strike3, 2)
