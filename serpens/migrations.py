60e459e57952b3a6578f3d856ddee51f6bf4e329
Everaldo Canuto <everaldo.canuto@gmail.com>
2021-12-21 23:29:09 -0300

    Fix default args for migrate func

--- serpens/migrations.py
+++ serpens/migrations.py
@@ -6,15 +6,19 @@ from yoyo import get_backend
 from serpens import envvars
 
 
-def migrate(uri, migrations_path):
+def migrate(uri=None, migrations_path=None):
+    if uri is None:
+        uri = envvars.get("DATABASE_URL")
+
+    if migrations_path is None:
+        migrations_path = envvars.get("DATABASE_MIGRATIONS_PATH", "./migrations")
+
     backend = get_backend(uri)
     migrations = read_migrations(migrations_path)
     backend.apply_migrations(backend.to_apply(migrations))
 
 
 def migrate_handler(event, context):
-    uri = envvars.get("DATABASE_URL")
-    path = envvars.get("DATABASE_MIGRATIONS_PATH", "./migrations")
     logging.info("Migrating database...")
-    migrate(uri, path)
+    migrate()
     logging.info("Migration successful")
