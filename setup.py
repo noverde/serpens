7f763db451626a91ac3a7fe11620f9dc0b77ade5
Everaldo Canuto <everaldo.canuto@gmail.com>
2021-12-28 20:12:22 -0300

    Upgrade version to 2.0.0a3 (alpha 3)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="serpens",
-    version="2.0.0a2",
+    version="2.0.0a3",
     description="A set of Python utilities, recipes and snippets",
     author="Everaldo Canuto",
     author_email="everaldo.canuto@gmail.com",
