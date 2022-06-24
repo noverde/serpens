e5b9d9c5bf0dfd061cc74a3e77cfa163564e3036
Jael Zela <jael.zela@noverde.com.br>
2022-06-24 16:13:06 -0300

    add key definition for Dict type

--- serpens/lambda_function.py
+++ serpens/lambda_function.py
@@ -1,9 +1,9 @@
 import json
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 import boto3
 
 
-def invoke(name: str, payload: Dict) -> Optional[Dict]:
+def invoke(name: str, payload: Dict[str, Any]) -> Optional[Dict[str, Any]]:
     client = boto3.client("lambda")
 
     response = client.invoke(FunctionName=name, Payload=json.dumps(payload))
