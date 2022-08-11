11b041eba6f7ac3a3435d2eb3c388ac6dc1a6bd3
Rodrigo Santiago <rodrigosantiag@gmail.com>
2022-08-11 10:25:38 -0300

    Format boto3 invoke response Add more tests

--- serpens/lambda_function.py
+++ serpens/lambda_function.py
@@ -1,9 +1,15 @@
 import json
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 import boto3
 
 
-def invoke(name: str, payload: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+def invoke(name: str, payload: Dict[str, Any]) -> Dict[str, Any]:
     client = boto3.client("lambda")
 
-    return client.invoke(FunctionName=name, Payload=json.dumps(payload))
+    response = client.invoke(FunctionName=name, Payload=json.dumps(payload))
+
+    return {
+        "status_code": response["StatusCode"],
+        "error": response.get("FunctionError"),
+        "payload": response["Payload"].read().decode(),
+    }
--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -12,35 +12,87 @@ class TestLambdaFunction(unittest.TestCase):
     def test_invoke_succeeded(self, m_boto3):
         payload_encoded = json.dumps({"key": "value"}).encode("utf-8")
         payload = StreamingBody(BytesIO(payload_encoded), len(payload_encoded))
+
         aws_response = {
+            "ResponseMetadata": {
+                "RequestId": "aae831be-0350-441f-9c0b-431e77682e44",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "date": "Wed, 10 Aug 2022 22:12:48 GMT",
+                    "content-type": "application/json",
+                    "content-length": "127",
+                    "connection": "keep-alive",
+                    "x-amzn-requestid": "aae831be-0350-441f-9c0b-431e77682e44",
+                    "x-amzn-remapped-content-length": "0",
+                    "x-amz-executed-version": "$LATEST",
+                    "x-amzn-trace-id": "root=1-62f42d60-38e7e0ac327c5e546868cc49;sampled=0",
+                },
+                "RetryAttempts": 0,
+            },
             "StatusCode": 200,
-            "FunctionError": "string",
             "LogResult": "string",
             "Payload": payload,
-            "ExecutedVersion": "string",
+            "ExecutedVersion": "$LATEST",
+        }
+
+        expected = {
+            "status_code": 200,
+            "error": None,
+            "payload": '{"key": "value"}',
         }
 
         m_boto3.client.return_value.invoke.return_value = aws_response
 
         response = lambda_function.invoke("function-test", {})
-        payload = json.loads(response["Payload"].read())
 
-        self.assertDictEqual(payload, {"key": "value"})
+        self.assertDictEqual(response, expected)
 
     @patch("lambda_function.boto3")
-    def test_invoke_failed(self, m_boto3):
-        payload_encoded = json.dumps({"error": "inkove error"}).encode("utf-8")
+    def test_invoke_with_lambda_error(self, m_boto3):
+        payload_encoded = json.dumps({"error": "invoke error"}).encode("utf-8")
         payload = StreamingBody(BytesIO(payload_encoded), len(payload_encoded))
+
         aws_response = {
-            "StatusCode": 500,
-            "FunctionError": "string",
+            "ResponseMetadata": {
+                "RequestId": "aae831be-0350-441f-9c0b-431e77682e44",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "date": "Wed, 10 Aug 2022 22:12:48 GMT",
+                    "content-type": "application/json",
+                    "content-length": "127",
+                    "connection": "keep-alive",
+                    "x-amzn-requestid": "aae831be-0350-441f-9c0b-431e77682e44",
+                    "x-amzn-remapped-content-length": "0",
+                    "x-amz-executed-version": "$LATEST",
+                    "x-amzn-trace-id": "root=1-62f42d60-38e7e0ac327c5e546868cc49;sampled=0",
+                },
+                "RetryAttempts": 0,
+            },
+            "StatusCode": 200,
             "LogResult": "string",
             "Payload": payload,
-            "ExecutedVersion": "string",
+            "ExecutedVersion": "$LATEST",
+            "FunctionError": "Unhandled",
+        }
+
+        expected = {
+            "status_code": 200,
+            "error": "Unhandled",
+            "payload": '{"error": "invoke error"}',
         }
 
         m_boto3.client.return_value.invoke.return_value = aws_response
 
-        with self.assertRaises(Exception):
-            response = lambda_function.invoke("function-test", {})
-            self.assertDictEqual(response, {"error": "inkove error"})
+        response = lambda_function.invoke("function-test", {})
+        self.assertDictEqual(response, expected)
+
+    @patch("lambda_function.boto3")
+    def test_invoke_with_exception(self, m_boto3):
+        payload_encoded = json.dumps({"error": "invoke error"}).encode("utf-8")
+        payload = StreamingBody(BytesIO(payload_encoded), len(payload_encoded))
+
+        m_boto3.client.return_value.invoke.side_effect = Exception("Error message")
+
+        with self.assertRaises(Exception) as error:
+            lambda_function.invoke("function-test", payload)
+            self.assertEqual(str(error), "Error message")
