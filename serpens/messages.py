f9c2cde5d04b7f219be8f0b5c9718a7d0367d25e
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2024-02-14 14:30:05 -0300

    feat: change name from url to destination queue

--- serpens/messages.py
+++ serpens/messages.py
@@ -12,12 +12,12 @@ class MessageProvider(Enum):
     SQS = "sqs"
 
 
-def send_message(message_url, message_body, message_group_id=None, message_attributes={}):
+def send_message(message_destination, message_body, message_group_id=None, message_attributes={}):
     logger.debug(f"Received message: {message_body}")
-    logger.debug(f"Received url: {message_url}")
+    logger.debug(f"Received destination: {message_destination}")
     logger.debug(f"Received attributes: {message_attributes}")
 
     if MESSAGE_PROVIDER == MessageProvider.SQS:
-        sqs.publish_message(message_url, message_body, message_group_id, message_attributes)
+        sqs.publish_message(message_destination, message_body, message_group_id, message_attributes)
     else:
         raise ValueError("Unsupported topic type or not configured")
