1bbf082be6a635fa0747df06589fc0ffb2a06898
Luis Henrique <henriqueluis3104@hotmail.com>
2024-02-15 17:45:16 -0300

    test: update tests

--- tests/test_pubsub.py
+++ tests/test_pubsub.py
@@ -1,31 +1,60 @@
+import json
 import unittest
 from unittest.mock import patch
 
 from google.api_core import exceptions
 from pubsub import publish_message
+from serpens.schema import SchemaEncoder
 
 
 class pubsub(unittest.TestCase):
     @patch("pubsub.pubsub_v1")
     def test_publish_message_succeeded(self, m_pubsub_v1):
-        expected_message_id = 123
-        attr = {"key": "value"}
-        message = "message"
+        items = (
+            {
+                "case": "data is not an instance of string",
+                "topic": "projects/myproject/topics/mytopic",
+                "data": {"foo": "bar"},
+                "attributes": {"foo": "bar"},
+            },
+            {
+                "case": "attribute is none",
+                "topic": "projects/myproject/topics/mytopic",
+                "data": "foo",
+                "attributes": None,
+            },
+            {
+                "case": "topic name have a endpoint",
+                "topic": "projects/myproject/topics/mytopic:myqueue",
+                "data": "foo",
+                "attributes": {"foo": "bar", "endpoint": "myqueue"},
+            },
+        )
 
+        expected_message_id = 123
         publisher = m_pubsub_v1.PublisherClient.return_value
         publisher.publish.return_value.result.return_value = expected_message_id
 
-        message_id = publish_message(
-            "projects/dotz-noverde-dev/topics/sre-playground", message, attributes=attr
-        )
+        for item in items:
+            with self.subTest(item["case"]):
+                message_id = publish_message(
+                    item["topic"], item["data"], attributes=item["attributes"]
+                )
 
-        self.assertEqual(message_id, expected_message_id),
-        publisher.publish.assert_called_once_with(
-            "projects/dotz-noverde-dev/topics/sre-playground",
-            data=message.encode("utf-8"),
-            ordering_key="",
-            **attr,
-        )
+                if item["attributes"] is None:
+                    item["attributes"] = {}
+
+                if not isinstance(item["data"], str):
+                    item["data"] = json.dumps(item["data"], cls=SchemaEncoder)
+
+                self.assertEqual(message_id, expected_message_id),
+
+                publisher.publish.assert_called_with(
+                    "projects/myproject/topics/mytopic",
+                    data=item["data"].encode("utf-8"),
+                    ordering_key="",
+                    **item["attributes"],
+                )
 
     @patch("pubsub.pubsub_v1")
     def test_publish_message_failed(self, m_pubsub_v1):
