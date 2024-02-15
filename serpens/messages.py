bf33637fca1014a1b0518b86a812bf9b2f4781a1
Juliano Fernandes <julianofernandes@gmail.com>
2024-02-15 12:33:02 -0300

    refactor: messages module

--- serpens/messages.py
+++ serpens/messages.py
@@ -1,25 +1,34 @@
+import importlib
 import logging
 import os
 from enum import Enum
-import sqs
+from typing import Any, Dict, Optional
 
 logger = logging.getLogger(__name__)
-
-MESSAGE_PROVIDER = os.getenv("MESSAGE_PROVIDER")
 
 
 class MessageProvider(Enum):
     SQS = "sqs"
 
 
-def publish_message(
-    message_destination, message_body, message_order_key=None, message_attributes={}
-):
-    logger.debug(f"Provider: {MESSAGE_PROVIDER}")
+class MessageClient:
+    _instance = None
 
-    if MESSAGE_PROVIDER == MessageProvider.SQS.value:
-        sqs.publish_message(
-            message_destination, message_body, message_order_key, message_attributes
-        )
-    else:
-        raise ValueError("Unsupported message provider type or not configured")
+    def __init__(self, provider: Optional[MessageProvider] = None):
+        self._provider = provider or MessageProvider(os.getenv("MESSAGE_PROVIDER"))
+        logger.debug(f"Provider: {self._provider.value}")
+        module = importlib.import_module(f"serpens.{self._provider.value}")
+        self._publish = module.publish_message
+
+    @classmethod
+    def publish(
+        cls,
+        destination: str,
+        body: Any,
+        order_key: Optional[str] = None,
+        attributes: Optional[Dict[str, str]] = None,
+    ) -> Dict[str, Any]:
+        if cls._instance is None:
+            cls._instance = cls()
+
+        return cls._instance._publish(destination, body, order_key, attributes)
